ea1e8af7d01fc72f0860e955a8cc2dfdec84e739
Mradul Makwana <mradul.makwana83@gmail.com>
2024-11-24 23:43:54 +0530

    add authentication

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,17 +1,50 @@
 import React from "react";
 import Link from "next/link";
-const Header = () => {
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  signOut,
+  withAuth,
+} from "@workos-inc/authkit-nextjs";
+
+const Header = async () => {
+  const { user } = await withAuth();
+  const signInUrl = await getSignInUrl();
+  // console.log(user);
+  const signOutUrl = await getSignUpUrl();
+
   return (
     <header>
       <div className=" container flex items-center  justify-between py-4  px-6 mx-auto my-4 ">
         <Link href={"/"} className="font-bold  text-xl ">
           Job Board
         </Link>
-        <nav className="flex gap-2  *:py-2 *:px-4 *:rounded-md">
-          <Link className="bg-gray-200" href={"/login"}>
-            Login
-          </Link>
-          <Link className="bg-blue-600 text-white " href={"/new-listing"}>
+        <nav className="flex gap-2">
+          {!user && (
+            <Link className="bg-gray-200 rounded-md py-2 px-4" href={signInUrl}>
+              Login
+            </Link>
+          )}
+
+          {user && (
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <button
+                type="submit"
+                className="bg-gray-200 rounded-md py-2 px-4"
+              >
+                Log out
+              </button>
+            </form>
+          )}
+          <Link
+            className="bg-blue-600 text-white rounded-md py-2 px-4"
+            href={"/new-listing"}
+          >
             Post a job
           </Link>
         </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+// import Image from "next/image";
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  withAuth,
+  signOut,
+} from "@workos-inc/authkit-nextjs";
 import Hero from "./Components/Hero";
 import React from "react";
 import Jobs from "./Components/Jobs";
 
-const page = () => {
+const page = async () => {
+  // Retrieves the user from the session or returns `null` if no user is signed in
+  const { user } = await withAuth();
+
+  // Get the URL to redirect the user to AuthKit to sign in
+  const signInUrl = await getSignInUrl();
+
+  // Get the URL to redirect the user to AuthKit to sign up
+  const signUpUrl = await getSignUpUrl();
   return (
     <>
       <Hero />
